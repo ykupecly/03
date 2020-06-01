8eaa201dd6bf976834f1f827fc24b0ce14255af3
unknown <yuecy2016@163.com>
2020-06-01 20:24:11 +0800

    作业1提交

---
+++ code/作业一/mvc/src/main/java/com/lagou/demo/controller/DemoController.java
@@ -0,0 +1,33 @@
+package com.lagou.demo.controller;
+
+import com.lagou.demo.service.IDemoService;
+import com.lagou.edu.mvcframework.annotations.LagouAutowired;
+import com.lagou.edu.mvcframework.annotations.LagouController;
+import com.lagou.edu.mvcframework.annotations.LagouRequestMapping;
+import com.lagou.edu.mvcframework.annotations.LagouSecurity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@LagouController
+@LagouRequestMapping("/demo")
+public class DemoController {
+
+
+    @LagouAutowired
+    private IDemoService demoService;
+
+
+    /**
+     * URL: /demo/query?name=lisi
+     * @param request
+     * @param response
+     * @param name
+     * @return
+     */
+    @LagouRequestMapping("/query")
+    @LagouSecurity({"zhangsan","lisi"})
+    public String query(HttpServletRequest request, HttpServletResponse response,String name) {
+        return demoService.get(name);
+    }
+}
